6a75cece16fc3ca0957a42272c7fbe7e17cb9baa
DianaBadas <62337245+DianaBadas@users.noreply.github.com>
2021-02-23 08:55:49 +0000

    Try different queries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    # @posts = Post.all
+    # @posts = Post.all.includes([:user])
     @posts = Post.all.includes(user: :address)
   end
 end
