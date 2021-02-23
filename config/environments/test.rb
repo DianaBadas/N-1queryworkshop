01c6086c1d5f5b0b73bed1176809b8cf87793557
DianaBadas <62337245+DianaBadas@users.noreply.github.com>
2021-02-23 08:53:09 +0000

    Add gem bullet

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,6 +5,11 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.after_initialize do
+      Bullet.enable        = true
+      Bullet.bullet_logger = true
+      Bullet.raise         = true # raise an error if n+1 query occurs
+    end
 
   config.cache_classes = false
   config.action_view.cache_template_loading = true
